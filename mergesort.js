d2246a8727bc81d844450bc1a246a99d8f3a01b6
Reed Branson <reed.branson@gmail.com>
2016-06-10 10:56:43 -0400

    completed sorting workshop

--- mergesort.js
+++ mergesort.js
@@ -1,33 +1,42 @@
 function split(wholeArray) {
+	if (wholeArray.length <= 1) {
+	return [wholeArray];
+	} else {
+		var cutoff = Math.floor(wholeArray.length / 2);
+		var firstHalf = wholeArray.slice(0,cutoff);
+		var secondHalf = wholeArray.slice(cutoff);
 
- var cutoff = Math.floor(wholeArray.length / 2);
- var firstHalf = wholeArray.slice(0,cutoff);
- var secondHalf = wholeArray.slice(cutoff);
-
-  return [firstHalf, secondHalf];
+	  return [firstHalf, secondHalf];
+	}
 }
 
 function merge(arr1, arr2) {
 	var masterArray = [];
-	if (!arr1.length) {
-		masterArray = masterArray.concat(arr2);
-		return masterArray;
-	}
-	if (!arr2.length) {
-		masterArray = masterArray.concat(arr1);
-		return masterArray;
-	}
-	if (arr1[0] < arr2[0]) {
-		masterArray.unshift(arr1[0])
-		arr1.shift();
-	} else {
-		masterArray.unshift(arr2[0])
-		arr2.shift();
-	}
+		if (!arr1.length) {
+			masterArray = masterArray.concat(arr2);
+			return masterArray;
+		}
+		if (!arr2.length) {
+			masterArray = masterArray.concat(arr1);
+			return masterArray;
+		}
+		if (arr1[0] < arr2[0]) {
+			masterArray.unshift(arr1[0])
+			arr1.shift();
+		} else {
+			masterArray.unshift(arr2[0])
+			arr2.shift();
+		}
 	return masterArray.concat(merge(arr1, arr2));
 }
 
 
 function mergeSort(array) {
-	
+	console.log(array);
+	if (array.length <= 1) {
+		return array;
+	} else {
+		var splitArr = split(array);
+		return merge(mergeSort(splitArr[0]), mergeSort(splitArr[1]));
+	}
 }
--- mergsort.spec.js
+++ mergsort.spec.js
@@ -1,3 +1,41 @@
+describe('Split', function(){
+  it('handles an empty array', function(){
+    expect( split([]) ).toEqual( [[]] );
+  });
+
+  it('handles a single item array', function(){
+    expect( split([1]) ).toEqual( [[1] ]);
+  });
+
+  it('splits an array with an even length', function(){
+    expect( split([2,3,5,4,1,6]) ).toEqual( [[2,3,5], [4,1,6] ]);
+  });
+
+  it('splits an array with an odd length', function(){
+    expect( split([2,3,5,4,1]) ).toEqual( [[2,3], [5,4,1] ]);
+  });
+
+});
+
+describe('Merge', function(){
+
+  it('handles an empty array', function(){
+    expect( merge([2,3], []) ).toEqual( [2,3]);
+  });
+
+  it('handles two empty arrays', function(){
+    expect( merge([], []) ).toEqual( []);
+  });
+
+  it('combines two arrays of equal size in order', function(){
+    expect( merge([1,9], [3, 7]) ).toEqual( [1,3,7,9]);
+  });
+
+  it('combines two arrays of differing size in order', function(){
+    expect( merge([1,2], [1, 2, 5]) ).toEqual( [1,1,2,2,5]);
+  });
+});
+
 describe('Merge Sort', function(){
   it('handles an empty array', function(){
     expect( mergeSort([]) ).toEqual( [] );
@@ -9,17 +47,5 @@ describe('Merge Sort', function(){
 
   it('handles a multiple item array', function(){
     expect( mergeSort([2,3,5,4,1]) ).toEqual( [1,2,3,4,5] );
-  });
-});
-
-describe('Split', function(){
-  it('handles an empty array', function(){
-    expect( split([2,3,5,4,1]) ).toEqual( [[2,3], [5,4,1] ]);
-  });
-});
-
-describe('Merge', function(){
-  it('combines two arrays in order', function(){
-    expect( merge([2,3], [1, 4, 5]) ).toEqual( [1,2,3,4,5]);
   });
 });
